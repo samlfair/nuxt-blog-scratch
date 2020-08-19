8fa2559f33aa2e35b92db3b541a8d880092ec32a
Sam Littlefair <sam.littlefair@prismic.io>
2020-08-19 18:31:12 +0200

    Add SEO error handling to posts

--- pages/post/_uid.vue
+++ pages/post/_uid.vue
@@ -29,9 +29,41 @@ export default {
     }
   },
   head() {
-    return {
-      title: this.$prismic.asText(this.postContent.title) + " - Sam Littlefair",
-      meta: [
+    const meta = [
+      {
+        property: "og:url",
+        content: process.env.baseUrl + this.$route.path
+      },
+      {
+        property: "og:site_name",
+        content: "Sam Littlefair"
+      },
+      {
+        name: "twitter:creator",
+        content: "@samlfair"
+      },
+      {
+        name: "twitter:url",
+        content: process.env.baseUrl + this.$route.path
+      },
+      {
+        name: "twitter:title",
+        content:
+          (this.postContent.meta_title[0].text ||
+            this.$prismic.asText(this.postContent.title)) + " - Sam Littlefair"
+      },
+      {
+        name: "twitter:dnt",
+        content: "off"
+      },
+      {
+        itemprop: "name",
+        content: this.postContent.meta_title[0].text
+      }
+    ];
+
+    if (this.postContent.meta_description[0].text) {
+      meta.push(
         {
           property: "description",
           content: this.postContent.meta_description[0].text
@@ -41,32 +73,20 @@ export default {
           content: this.postContent.meta_description[0].text
         },
         {
+          itemprop: "description",
+          content: this.postContent.meta_description[0].text
+        }
+      );
+    }
+    if (this.postContent.meta_image.url) {
+      meta.push(
+        {
           property: "og:image",
           content: this.postContent.meta_image.url
         },
         {
           property: "og:image:alt",
           content: this.postContent.meta_image.alt
-        },
-        {
-          property: "og:url",
-          content: process.env.baseUrl + this.$route.path
-        },
-        {
-          property: "og:site_name",
-          content: "Sam Littlefair"
-        },
-        {
-          name: "twitter:creator",
-          content: "@samlfair"
-        },
-        {
-          name: "twitter:url",
-          content: process.env.baseUrl + this.$route.path
-        },
-        {
-          name: "twitter:title",
-          content: this.postContent.meta_title[0].text
         },
         {
           name: "twitter:image",
@@ -77,22 +97,16 @@ export default {
           content: this.postContent.meta_image.url
         },
         {
-          name: "twitter:dnt",
-          content: "off"
-        },
-        {
-          itemprop: "name",
-          content: this.postContent.meta_title[0].text
-        },
-        {
-          itemprop: "description",
-          content: this.postContent.meta_description[0].text
-        },
-        {
           itemprop: "image",
           content: this.postContent.meta_image.url
         }
-      ]
+      );
+    }
+    return {
+      title:
+        (this.postContent.meta_title[0].text ||
+          this.$prismic.asText(this.postContent.title)) + " - Sam Littlefair",
+      meta
     };
   }
 };
