fb56ec587620dd654013d151f9e9cbec7ee6872a
Sam Littlefair <sam.littlefair@prismic.io>
2020-09-04 14:46:47 +0200

    Styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <slice-zone class="slice-zone" type="page" uid="home" />
     <div class="postlist">
       <div class="standard-feed">
         <div v-for="post in blogPosts" v-bind:key="post.id">
@@ -12,6 +13,7 @@
 
 <script>
 import Thumbnail from "~/components/Thumbnail";
+import SliceZone from "vue-slicezone";
 
 export default {
   name: "Home",
@@ -19,7 +21,8 @@ export default {
     return this.$seo(this.homeContent, this.$route.path);
   },
   components: {
-    Thumbnail
+    Thumbnail,
+    SliceZone
   },
   data: function() {
     return {
@@ -58,5 +61,41 @@ h1 {
 
 .standard-feed {
   border-bottom: 1px solid #eee;
+}
+
+.slice-zone {
+  background: $lightgrey;
+  padding: 16px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  color: $grey;
+  /deep/ .rich-text {
+    margin: 0;
+    a {
+      color: #6b6b6b;
+      text-shadow: none;
+      box-shadow: -2px -1px 0px 0px inset $lightgrey,
+        0px -8px 0px 0px inset $dimgrey;
+      padding-left: 2px;
+      margin-left: -2px;
+      &:hover {
+        box-shadow: -2px -1px 0px 0px inset $lightgrey,
+          0px -8px 0px 0px inset #dddddd;
+        padding-left: 2px;
+        margin-left: -2px;
+      }
+    }
+  }
+  &::before {
+    content: "ℹ︎";
+    margin-top: 2px;
+    margin-right: 15px;
+    margin-left: 2px;
+    border-radius: 50%;
+    align-self: flex-start;
+    padding: 3px 7px 6px 7px;
+    border: 2px solid $grey;
+  }
 }
 </style>
